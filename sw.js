0464bda87e2988f02c9372565f0a06cb64cf5bc4
Anne Ernst <anne.ernst@eraneos.com>
2026-06-26 08:31:09 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'maus-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('videos.json')) {
+    // network-first so the list can be updated without reinstalling
+    e.respondWith(fetch(e.request).then((r) => { const cc = r.clone(); caches.open(CACHE).then((c) => c.put(e.request, cc)); return r; }).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  }
+});
